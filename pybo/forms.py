dd5c8a6b03a2f226d9e1d1cb44034a4e857431b3
huji126 <huji126@gmail.com>
2021-07-15 13:19:01 +0900

    소스 추가

---
+++ pybo/forms.py
@@ -0,0 +1,20 @@
+from .models import Question, Answer
+from django import forms
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['subject', 'content']
+        labels = {
+            'subject': '제목',
+            'content': '내용'
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용'
+        }
--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
 from .models import Question, Answer
 from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')  # "-" 기호 넣으면 내림차순
@@ -17,5 +17,29 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.question = question
+            answer.create_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def question_create(request):
+    # 질문 등록
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():   # 폼이 유효한지 검사
+            question = form.save(commit=False)  # 임시저장
+            question.create_date = timezone.now()
+            question.save()  # 진짜 저장
+            return redirect('pybo:index')
+    else:  # request.method == 'GET'
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
