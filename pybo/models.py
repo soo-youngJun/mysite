7a8650e4258e6a2bf8a3e7754a2ef60e5867f03a
soo-youngJun <huji126@gmail.com>
2021-07-23 13:16:44 +0900

    add search

--- pybo/models.py
+++ pybo/models.py
@@ -3,23 +3,27 @@ from django.contrib.auth.models import User
 
 # 질문 모델(테이블)
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     def __str__(self):
         return self.subject
 
 # 답변 모델(테이블)
 class Answer(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     # 외래키 제약조건 무시하고 연쇄 삭제 됨
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
     def __str__(self):
         return self.content
--- pybo/views.py
+++ pybo/views.py
@@ -5,6 +5,7 @@ from .forms import QuestionForm, AnswerForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 def index(request):
     return render(request, 'pybo/index.html')
@@ -16,16 +17,22 @@ def board(request):
     # 질문 목록
     # 127.0.0.1:8000/pybo/board/?page=1
     page = request.GET.get('page', '1')
-
+    kw = request.GET.get('kw', '')       # 검색어 전달
     # 조회
     question_list = Question.objects.order_by('-create_date')
-
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |        # 제목 검색 *"i"contains는 대소문자 다 가능
+            Q(content__icontains=kw) |        # 내용 검색
+            Q(author__username__icontains=kw) |       # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)       # 답변 글쓴이 검색
+        ).distinct()  # 중복제거
     # 페이징 처리 -페이지당 10개씩 보여줌
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list' : page_obj}
-    return render(request, 'pybo/question_list.html' ,context)
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
     #question = Question.objects.get(id=question_id)
