dc3883a9a39c31d0369967ea47455be31e42fd7f
soo-youngJun <huji126@gmail.com>
2021-07-28 09:49:22 +0900

    배포준비

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*+k&nt)x8itt%3ph@^rom5ldera^xnwqnku!v!s*rk^#5bqtx)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
